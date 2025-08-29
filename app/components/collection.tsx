85fb4930e8a80279847308de31881c6011fa858b
Md Al Amin Islam <mdalamin212104>
2025-08-29 17:51:11 +0600

    code formeting done

--- app/components/collection.tsx
+++ app/components/collection.tsx
@@ -1,15 +1,11 @@
-import { Suspense } from "react";
-import { Await } from "react-router";
-import { ProductItem } from "./ProductItem";
+import {Suspense} from 'react';
+import {Await} from 'react-router';
+import {ProductItem} from './ProductItem';
 
-export function CollectionProducts({
-  products,
-}: {
-  products: Promise<any>;
-}) {
+export function CollectionProducts({products}: {products: Promise<any>}) {
   return (
     <div className="collection-products">
-      <h2 className='bg-blue-500 text-white p-10'>Collection Products</h2>
+      <h2 className="bg-blue-500 text-white p-10">Collection Products</h2>
       <Suspense fallback={<div>Loading...</div>}>
         <Await resolve={products}>
           {(response) => (
